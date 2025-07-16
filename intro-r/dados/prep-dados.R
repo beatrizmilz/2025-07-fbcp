3808413b97524a215c8d98f81f8c8c82d78cfc53
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-07-16 17:57:44 -0300

    documenta, salva em csv2

--- intro-r/dados/prep-dados.R
+++ intro-r/dados/prep-dados.R
@@ -1,13 +1,19 @@
-library(readr)
+# Carregar pacote
 library(tidyverse)
+
+# Importar dados ------------------------------------------------------
 # Dados baixados de https://dadosabertos.tse.jus.br/dataset/resultados-2024
-votacao_candidato_munzona_2024_SP <- read_delim("intro-r/dados/votacao_candidato_munzona_2024_SP.csv", 
-    delim = ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
-    trim_ws = TRUE)
-View(votacao_candidato_munzona_2024_SP)
+votacao_candidato_munzona_2024_SP <- read_delim(
+  "intro-r/dados/votacao_candidato_munzona_2024_SP.csv",
+  delim = ";",
+  escape_double = FALSE,
+  locale = locale(encoding = "ISO-8859-1"),
+  trim_ws = TRUE
+)
 
-
+# Filtrar dados para o município de São Paulo ----------------------
 votacao_muni_sp <- votacao_candidato_munzona_2024_SP |> 
   filter(NM_MUNICIPIO == "SÃO PAULO") 
 
-write_csv(votacao_muni_sp, "intro-r/dados/votacao_muni_sp.csv")
+# Salvar dados ------------------------------------------------------
+write_csv2(votacao_muni_sp, "intro-r/dados/votacao_muni_sp.csv")
